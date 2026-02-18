7a7db9b441feee6d6b428a456a83d9856e89ab2d
Himanshu-Goyal21 <goyalhimanshu.hgs@gmail.com>
2026-02-18 10:20:23 +0000

    Implement StackADT interface to fix compilation errors

--- src/main/java/StackADT.java
+++ src/main/java/StackADT.java
@@ -1,1 +1,22 @@
+public interface StackADT<E> {
+    int size();
+    boolean isEmpty();
 
+    /**
+     * Returns the top element without removing it.
+     * If empty, returns null.
+     */
+    E top();
+
+    /**
+     * Adds an element to the top of the stack.
+     * If the stack is full (capacity), throw IllegalStateException.
+     */
+    void push(E e);
+
+    /**
+     * Removes and returns the top element.
+     * If empty, returns null.
+     */
+    E pop();
+}
